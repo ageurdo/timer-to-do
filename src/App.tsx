81b45e4a496ad1cd2ec80785769bffff2a18ddfc
Ageu Rodrigues <ageu_rdo@hotmail.com>
2023-02-25 18:36:29 -0300

    Timmer working

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+import { CyclesContextProvider } from './contexts/CyclesContext'
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
+  )
+}
