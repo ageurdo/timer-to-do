378c12aae39d25b12f98e2626dc723d0dda226ed
Ageu Rodrigues <ageu_rdo@hotmail.com>
2023-03-01 16:18:31 -0300

    Added configs to run in Github Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from 'styled-components'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter as BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
